a2eb7911e6e6957ca792285952c111b2938cc6a4
caioalonso827 <caioca827@gmail>
2026-07-14 21:45:58 -0300

    Ajeitando Ponto

--- src/main/java/com/pulsoetico/pulsoetico/models/dtos/RegistroPontoResponse.java
+++ src/main/java/com/pulsoetico/pulsoetico/models/dtos/RegistroPontoResponse.java
@@ -1,15 +1,38 @@
 package com.pulsoetico.pulsoetico.models.dtos;
 
-import com.pulsoetico.pulsoetico.models.RegistroPonto;
-
 import java.time.Instant;
+
+import com.pulsoetico.pulsoetico.models.RegistroPonto;
 
 public record RegistroPontoResponse(
         Long id,
+        Long empresaId,
+        String empresaNome,
+        Long setorId,
+        String setorNome,
         RegistroPonto.TipoRegistro tipo,
         Instant horario
 ) {
-    public static RegistroPontoResponse from(RegistroPonto registro) {
-        return new RegistroPontoResponse(registro.getId(), registro.getTipo(), registro.getHorario());
+
+    public static RegistroPontoResponse from(
+            RegistroPonto registro
+    ) {
+        return new RegistroPontoResponse(
+                registro.getId(),
+                registro.getEmpresa() != null
+                        ? registro.getEmpresa().getId()
+                        : null,
+                registro.getEmpresa() != null
+                        ? registro.getEmpresa().getNome()
+                        : null,
+                registro.getSetor() != null
+                        ? registro.getSetor().getId()
+                        : null,
+                registro.getSetor() != null
+                        ? registro.getSetor().getNome()
+                        : null,
+                registro.getTipo(),
+                registro.getHorario()
+        );
     }
 }
